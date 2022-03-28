b7cbbc89a32dc3e68b900bd7e5eb660c632b581c
antwe841 <antwe841@muxen2-013.ad.liu.se>
2022-03-28 15:32:14 +0200

    Added stack pointer and some comments

--- src/pipeCPU.vhd
+++ src/pipeCPU.vhd
@@ -11,34 +11,38 @@ end pipeCPU;
 
 architecture func of pipeCPU is
 
-signal IR1 : unsigned(31 downto 0);
-alias IR1_op : unsigned(5 downto 0) is IR1(31 downto 26);
-alias IR1_d : unsigned(4 downto 0) is IR1(25 downto 21);
-alias IR1_a : unsigned(4 downto 0) is IR1(20 downto 16);
-alias IR1_b : unsigned(4 downto 0) is IR1(15 downto 11);
-alias IR1_c : unsigned(10 downto 0) is IR1(10 downto 0);
+signal IR1 : unsigned(25 downto 0); -- Fetch stage
+alias IR1_op : unsigned(5 downto 0) is IR1(25 downto 20);
+alias IR1_rd : unsigned(3 downto 0) is IR1(19 downto 16);
+alias IR1_ra : unsigned(3 downto 0) is IR1(15 downto 12);
+alias IR1_const : unsigned(11 downto 0) is IR1(11 downto 0);
 
-signal IR2 : unsigned(31 downto 0);
-alias IR2_op : unsigned(5 downto 0) is IR2(31 downto 26);
-alias IR2_d : unsigned(4 downto 0) is IR2(25 downto 21);
-alias IR2_a : unsigned(4 downto 0) is IR2(20 downto 16);
-alias IR2_b : unsigned(4 downto 0) is IR2(15 downto 11);
-alias IR2_c : unsigned(10 downto 0) is IR2(10 downto 0);
+signal IR2 : unsigned(25 downto 0); -- Decode stage
+alias IR2_op : unsigned(5 downto 0) is IR2(25 downto 20);
+alias IR2_rd : unsigned(3 downto 0) is IR2(19 downto 16);
+alias IR2_ra : unsigned(3 downto 0) is IR2(15 downto 12);
+alias IR2_const : unsigned(11 downto 0) is IR2(11 downto 0);
+
+-- Stack pointer
+signal SP : unsigned(15 downto 0);
 
 signal PC, PC1, PC2 : unsigned(10 downto 0);
 
 signal PMdata_out : unsigned(31 downto 0);
 signal pm_addr : unsigned(8 downto 0);
 
+-- Instructions
 constant iNOP : unsigned(5 downto 0) := "000000";
 constant iJ 	: unsigned(5 downto 0) := "010101";
 constant iBF 	: unsigned(5 downto 0) := "000100";
+constant iPUSH 	: unsigned(5 downto 0) := "000111"; -- TODO change op code
+constant iPOP 	: unsigned(5 downto 0) := "000110";
 
 component PM_comp is
 	port(
 		addr : in unsigned(8 downto 0);
 		data_out : out unsigned(31 downto 0)
-		);	
+		);
 end component;
 
 
@@ -49,6 +53,7 @@ begin
 		data_out => PMdata_out
 	);
 
+	-- If jmp instruction, take value from IR2, else increment
 	process(clk)
 	begin
 		if rising_edge(clk) then
@@ -60,10 +65,11 @@ begin
 				PC <= PC + 1;
 			end if;
 		end if;
-	end process;	
-	
+	end process;
+
 	pm_addr <= PC(8 downto 0);
-	
+
+	-- Update PC1. Copy PC
 	process(clk)
 	begin
 		if rising_edge(clk) then
@@ -73,19 +79,21 @@ begin
 				PC1 <= PC;
 			end if;
 		end if;
-	end process;	
+	end process;
 
+	-- Update PC2, jump if needed
 	process(clk)
 	begin
 		if rising_edge(clk) then
 			if (rst='1') then
 				PC2 <= (others => '0');
 			else
-				PC2 <= PC1 + IR1_c;
+				PC2 <= PC1 + IR1_const;
 			end if;
 		end if;
-	end process;	
-	
+	end process;
+
+	-- Load IR1 NOP if jmp instruction, else read from PM
 	process(clk)
 	begin
 		if rising_edge(clk) then
@@ -99,7 +107,7 @@ begin
 		end if;
 	end process;
 
-	
+	-- Update IR2, copies IR1
 	process(clk)
 	begin
 		if rising_edge(clk) then
@@ -111,5 +119,21 @@ begin
 		end if;
 	end process;
 
-	
+	-- Update stack pointer
+	-- If push: decrement
+	-- If pop: increment
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			if (rst='1') then
+				SP <= (others => '0');
+			elsif (IR1_op = iPOP) then
+				SP <= SP + 1;
+			elsif (IR1_op = iPUSH) then
+				SP <= SP - 1;
+			end if;
+		end if;
+	end process;
+
+
 end architecture;
