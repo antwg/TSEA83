294c9df87148c03c4761f0e33a79e43fc1bc6157
r0x57n <contact@0x57.me>
2022-03-30 16:24:26 +0200

    for fast debugging

---
+++ assembler/printOutAsBin.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+with open("out.bin", 'rb') as f:
+    count = 0
+
+    while 1:
+        byte_s = f.read(1)
+        if not byte_s:
+            break
+
+        binary_s = "{:08b}".format(int(byte_s.hex(), 16))
+        count += 1
+        if count == 4:
+            print(binary_s[::-8])
+            count = 0
+        else:
+            print(binary_s[::-1], end='')
