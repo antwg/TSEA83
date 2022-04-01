12a9151752658f76ee4801059359b17733064cc9
Rasmus Wallin <raswa151@student.liu.se>
2022-04-01 10:27:00 +0200

    32bit instructions

--- src/MEM/PROG_MEM.vhd
+++ src/MEM/PROG_MEM.vhd
@@ -4,22 +4,22 @@ use IEEE.numeric_std.all;
 
 entity PROG_MEM is
 	Port( 	addr : in unsigned(15 downto 0);
-		data_out : out unsigned(25 downto 0);
+		data_out : out unsigned(31 downto 0);
 		clk, we : in std_logic;
 		wr_addr : in unsigned(15 downto 0);
-		wr_data : in unsigned(25 downto 0));
+		wr_data : in unsigned(31 downto 0));
 end PROG_MEM;
 
 architecture func of PROG_MEM is
 
-	type PM_t is array(0 to 4095) of unsigned(25 downto 0);
+	type PM_t is array(0 to 4095) of unsigned(31 downto 0);
 	constant PM_c : PM_t := (
-		"00100111111111111111111110",	-- dummy
-		"00110100100000000000000000",	-- dummy
-		"00000111111111111111111110",	-- dummy
-		"10000000000000000000000000",	-- dummy
-		"10000000000000000000000000",	-- dummy
-		"10000000000000000000000000",	-- dummy
+		"00100111111111111111111110100000",	-- dummy
+		"00110100100000000000000000100000",	-- dummy
+		"00000111111111111111111110100000",	-- dummy
+		"10000000000000000000000000100000",	-- dummy
+		"10000000000000000000000000100000",	-- dummy
+		"10000000000000000000000000100000",	-- dummy
 
 		others => (others => '0')
 	);
