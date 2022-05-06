5c5aca42088b587fc69ebbcb3ffba849469d482c
antwe841 <antwe841@muxen2-007.ad.liu.se>
2022-05-06 13:46:10 +0200

    Debugging UART

--- src/MEM/PROG_MEM.vhd
+++ src/MEM/PROG_MEM.vhd
@@ -14,25 +14,6 @@ architecture func of PROG_MEM is
 
 	type PM_t is array(0 to 1024) of unsigned(31 downto 0);
 	constant PM_c : PM_t := (
-		x"00000000", -- NOP
-		x"0810001F", -- LDI b,$001F  
-		x"0820401F", -- LDI c,$401F 
-		x"0830FC00", -- LDI d,$FC00 
-		x"0840FC01", -- LDI e,$FC01
-		x"0B310000", -- ST d,b
-		x"0B420000", -- ST e,c
-		x"0810001F", -- LDI b,$001F
-		x"0820401F", -- LDI c,$401F 
-		x"0830FC02", -- LDI d,$FC02
-		x"0840FC03", -- LDI e,$FC03
-		x"0B310000", -- ST d,b
-		x"0B420000", -- ST e,c
-		x"0850FC1F", -- LDI f,$FC1F
-		x"09050000", -- LD a,f
-		x"0A500000", -- STI f,$0000
-		x"0100FFFE", -- RJMP -2
-		
-		
 
 	    others => (others => '0')
 	);
