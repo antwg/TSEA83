b34a45346014b2cf752bb4da8eea80aca6cd0c6b
antwe841 <antwe841@muxen1-009.ad.liu.se>
2022-05-10 18:06:24 +0200

    forgot one change

--- src/pipeCPU.vhd
+++ src/pipeCPU.vhd
@@ -495,7 +495,7 @@ begin
     dm_and_sm_data_out <= dm_data_out when (alu_out < x"FC00") else sm_data_out;
       
 	-- Address controller
-	dm_addr <= (alu_out and "0000001111111111"); -- Currently only allow 256 addresses
+	dm_addr <= (alu_out and "0000001111111111"); -- Currently only allow 1024 addresses
 	dm_we <= '1' when ((alu_out < x"FC00") and ((IR2_op = STI) 		or 
 												(IR2_op = ST) 		or 
 												(IR2_op = PUSH) 	or 
